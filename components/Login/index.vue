dbb3c17eb81547b0d62914cb7ada957b91ea9594
dvigo <david.vigoesteban@gmail.com>
2020-11-29 01:22:36 +0100

    Refactor login

--- components/Login/index.vue
+++ components/Login/index.vue
@@ -1,9 +1,15 @@
 <template lang="pug">
-  v-form.login(@submit="login()")
-    input.input(placeholder="Tu correo electrónico" type="email" required ref="email")
-    input.input(placeholder="Tu contraseña" type="password" required ref="password")
-    .button
-      v-btn.font-weight-black(color='primary' type="submit") Enviar
+  v-flex(class='text-xs-center login') 
+    .display-1.mb-3 #[v-icon.mr-2(large) work] MyWorkspace
+    v-card(light)
+      v-card-text
+        .subheading
+          template(v-if='true') Log in to your customer portal
+          template(v-else) Crate a new account
+        v-form
+          input.input(placeholder="Tu correo electrónico" type="email" required ref="email")
+          input.input(placeholder="Tu contraseña" type="password" required ref="password")
+          v-btn(@click.prevent='login' block type='submit') Sign in
 </template>
 
 <script>
@@ -17,7 +23,7 @@ export default {
             username: this.$refs.email.value,
             password: this.$refs.password.value
           }
-        }).then((res) => console.log(res))
+        }).then((res) => console.log(res.data.user))
       } catch (e) {
         this.error = e.response.data.message
       }
