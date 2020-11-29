a828f78cb182474c5efefbf23b7a7f7e77853e88
dvigo <david.vigoesteban@gmail.com>
2020-11-29 02:57:49 +0100

    Add radar

---
+++ components/Radar/index.vue
@@ -0,0 +1,33 @@
+<template lang="pug">
+  v-container
+    v-row(sm="12")
+      h3 ¿Qué es Radar COVID?
+      p Radar COVID es una aplicación móvil desarrollada para ayudar a controlar la propagación de la COVID-19 a
+        | través de la identificación de los posibles contactos estrechos de casos confirmados a través de la tecnología
+        | Bluetooth.
+    v-row(sm="12")
+      v-col(sm="9")
+        v-img(:src="radarcovid")
+      v-col(sm="3")
+        a( href='https://apps.apple.com/es/app/radar-covid/id1520443509' target="_blank")
+          v-img.apple(:src="apple")
+        a( href="https://play.google.com/store/apps/details?id=es.gob.radarcovid&hl=es&gl=US" target="_blank")
+          v-img.android(:src="android" href="https://play.google.com/store/apps/details?id=es.gob.radarcovid&hl=es&gl=US" target="_blank")
+
+</template>
+
+<script>
+import radarcovid from '~/assets/img/radarcovid.jpg'
+import android from '~/assets/img/android.png'
+import apple from '~/assets/img/apple.png'
+
+export default {
+  data () {
+      return {
+        radarcovid,
+        android,
+        apple
+      }
+  }
+}
+</script>
---
+++ pages/radar.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+  v-app
+    Radar
+</template>
+
+<script>
+  import Radar from '~/components/Radar/index.vue'
+
+  export default {
+    components: {
+      Radar
+    }
+  }
+</script>
